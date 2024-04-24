636a0ca7b8f45e3478b0f1f9271f29fa5f2d8a14
Benny Gaechter <benny.gaechter@gmail.com>
2024-04-24 15:55:59 +0200

    initial implementation

---
+++ include/battlesnake.h
@@ -0,0 +1,135 @@
+//
+// Created by Benny Gaechter on 24.04.2024.
+//
+
+#ifndef BATTLESNAKE_H
+#define BATTLESNAKE_H
+#include "json.h"
+#include <string>
+using json = nlohmann::json;
+
+namespace battlesnake {
+    class Coord {
+    public:
+        explicit Coord(json coord);
+
+    private:
+        int x;
+        int y;
+    };
+
+    class Customizations {
+    public:
+        explicit Customizations(json customizations);
+
+    private:
+        std::string color;
+        std::string head;
+        std::string tail;
+    };
+
+    class Snake {
+    public:
+        explicit Snake(json snake);
+
+    private:
+        std::string id;
+        std::string name;
+        int health;
+        std::vector<Coord> body;
+        Coord head;
+        int length;
+        std::string latency;
+        std::string shout;
+        Customizations customizations;
+    };
+
+    class Board {
+    public:
+        explicit Board(json board);
+
+    private:
+        int height;
+        int width;
+        std::vector<Coord> food;
+        std::vector<Coord> hazards;
+        std::vector<Snake> snakes;
+    };
+
+    class RulesetSettings {
+    public:
+        explicit RulesetSettings(json rulesetSettings);
+
+    private:
+        int foodspawnChance;
+        int minimumFood;
+        int hazardDamagePerTurn;
+    };
+
+    class Ruleset {
+    public:
+        explicit Ruleset(json ruleset);
+
+    private:
+        std::string name;
+        std::string version;
+        RulesetSettings settings;
+    };
+
+    class Game {
+    public:
+        explicit Game(json game);
+
+    private:
+        std::string id;
+        Ruleset ruleset;
+        std::string map;
+        std::string source;
+        int timeout;
+    };
+
+    class GameState {
+    public:
+        explicit GameState(json state);
+
+    private:
+        Game game;
+        int turn;
+        Board board;
+        Snake you;
+    };
+
+    class Move {
+    };
+
+    class Info {
+    public:
+        Info();
+
+        Info(const std::string &apiVersion, const std::string &author, const std::string &color,
+             const std::string &head, const std::string &tail);
+
+        std::string GetInfo() const;
+
+    private:
+        json info;
+    };
+
+    class BattleSnake {
+    public:
+        BattleSnake();
+
+        std::string GetInfo() const;
+
+        std::string Start();
+
+        std::string Move(json state);
+
+        std::string End();
+
+    private:
+        Info info;
+    };
+} // battlesnake
+
+#endif //BATTLESNAKE_H
---
+++ src/battlesnake.cpp
@@ -0,0 +1,116 @@
+//
+// Created by Benny Gaechter on 24.04.2024.
+//
+
+#include "battlesnake.h"
+
+#include <utility>
+
+namespace battlesnake {
+    BattleSnake::BattleSnake() {
+        Info const i{};
+        info = i;
+    }
+
+    std::string BattleSnake::GetInfo() const {
+        return info.GetInfo();
+    }
+
+    std::string BattleSnake::End() {
+        return "End";
+    }
+
+    std::string BattleSnake::Move(json state) {
+        auto const gameState = GameState(std::move(state));
+
+        //TODO: Decide how to move
+
+        // Create response object
+        json response{};
+        response["move"] = "up";
+        response["shout"] = "Just going up";
+
+        return response.dump();
+    }
+
+    std::string BattleSnake::Start() {
+        return "";
+    }
+
+
+    Coord::Coord(json coord): x(coord["x"]), y(coord["y"]) {
+    }
+
+    Board::Board(json board): height(board["height"]), width(board["width"]) {
+        for (const auto &foodCoordinates: board["food"]) {
+            food.emplace_back(foodCoordinates);
+        }
+        for (const auto &hazardCoordinates: board["hazards"]) {
+            hazards.emplace_back(hazardCoordinates);
+        }
+        for (const auto &snake: board["snakes"]) {
+            snakes.emplace_back(snake);
+        }
+    }
+
+    Snake::Snake(json snake): head(snake["head"]), customizations(snake["customizations"]) {
+        id = snake["id"];
+        name = snake["name"];
+        health = snake["health"];
+        for (const auto &snakeBody: snake["body"]) {
+            body.emplace_back(snakeBody);
+        }
+        latency = snake["latency"];
+        length = snake["length"];
+        shout = snake["shout"];
+    }
+
+    RulesetSettings::RulesetSettings(json rulesetSettings): foodspawnChance(rulesetSettings["foodSpawnChance"]),
+                                                            minimumFood(rulesetSettings["minimumFood"]),
+                                                            hazardDamagePerTurn(
+                                                                rulesetSettings["hazardDamagePerTurn"]) {
+    }
+
+    Ruleset::Ruleset(json ruleset): settings(ruleset["settings"]) {
+        name = ruleset["name"];
+        version = ruleset["version"];
+    }
+
+    Game::Game(json game): ruleset(game["ruleset"]) {
+        id = game["id"];
+        map = game["map"];
+        source = game["source"];
+        timeout = game["timeout"];
+    }
+
+    GameState::GameState(json state): game(state["game"]), board(state["board"]), you(Snake{state["you"]}) {
+        turn = state["turn"];
+    }
+
+    Customizations::Customizations(json customizations) {
+        color = customizations["color"];
+        head = customizations["head"];
+        tail = customizations["tail"];
+    }
+
+    Info::Info() {
+        info["APIVersion"] = "1";
+        info["Author"] = "C++";
+        info["Color"] = "#c0ffee";
+        info["Head"] = "default";
+        info["Tail"] = "default";
+    }
+
+    Info::Info(const std::string &apiVersion, const std::string &author, const std::string &color,
+               const std::string &head, const std::string &tail) {
+        info["APIVersion"] = apiVersion;
+        info["Author"] = author;
+        info["Color"] = color;
+        info["Head"] = head;
+        info["Tail"] = tail;
+    }
+
+    std::string Info::GetInfo() const {
+        return info.dump();
+    }
+} // battlesnake
